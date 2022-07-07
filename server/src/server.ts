8c07724da7fc61504d5ef19208c7d4262c79a7a0
Moisés Neto <moisesnetored@gmail.com>
2022-07-07 01:04:53 -0400

    feat: Database connection configured.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-  console.log("Listagem de usuários")
-
-  response.json([
-    'Moises',
-    'Neto'
-  ])
-});
+app.use(routes);
 
 app.listen(3333);
