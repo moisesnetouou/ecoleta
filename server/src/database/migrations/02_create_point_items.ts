77e3c7bd5dc03ef77d5df88a942a9a65b84b6f66
Moisés <moisesnetored@gmail.com>
2022-07-07 08:38:38 -0400

    feat: Migrations created.

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,7 +1,13 @@
-export async function up() {
+import { Knex } from "knex";
 
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+    table.integer('point_id').references('id').inTable('points').notNullable;
+    table.integer('item_id').references('id').inTable('items').notNullable;
+  })
 }
 
-export async function down() {
-
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
 }
