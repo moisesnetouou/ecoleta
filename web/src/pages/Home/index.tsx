87de71d35b2a4b2c0855046b011a54717aff303a
Moisés Neto <moisesnetored@gmail.com>
2022-07-07 23:01:03 -0400

    feat: Construction of the form's html.

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
@@ -20,14 +21,14 @@ export function Home(){
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
           </p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>
               Cadastre um ponto de coleta
             </strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
