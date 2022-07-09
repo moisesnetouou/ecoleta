77e0df4e33ba56f1697ef75fb54d9ce928fd6b3e
Moisés Neto <moisesnetored@gmail.com>
2022-07-09 14:25:18 -0400

    feat: Home screen created, fonts, splash screen and tsconfig configured and working.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Home } from './src/pages/Home';
+import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if(!fontsLoaded) {
+    //@ts-ignore
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar 
+        barStyle='dark-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
