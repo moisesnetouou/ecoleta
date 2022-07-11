c834779353d9347321da1c6335b71fea3308f56d
Moisés Neto <moisesnetored@gmail.com>
2022-07-10 20:56:13 -0400

    feat: Using Dropzone to save image, saving data on the backend.

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,7 @@ import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
+import { Dropzone } from '../../components/Dropzone';
 
 interface Item {
   id: number;
@@ -43,6 +44,8 @@ export function CreatePoint(){
   })
 
   const [selectedPosition, setSelectedPoistion] = useState<[number, number]>([0,0]);
+
+  const [selectedFile, setSelectedFile] = useState<File>()
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -118,22 +121,28 @@ export function CreatePoint(){
   async function handleSubmit(event: FormEvent){
     event.preventDefault();
 
+    console.log(selectedFile);
+
     const {name, email, whatsapp} = formData;
     const uf = selectedUf;
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    }
+    const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      
+      if(selectedFile) {
+        data.append('image', selectedFile);
+      }
 
     await api.post('points', data);
     alert('Ponto de coleta criado!')
@@ -154,6 +163,8 @@ export function CreatePoint(){
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
